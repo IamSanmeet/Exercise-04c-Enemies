ef0b107cd0f7ae29da83a26ee0a088da0b993f5d
Your Name <youremail@example.com>
2023-04-10 17:35:51 -0400

    done

--- Enemies/Golem_Die.gd
+++ Enemies/Golem_Die.gd
@@ -5,8 +5,9 @@ onready var enemy = get_node("../..")
 
 func _ready():
 	yield(enemy, "ready")
-
+	
 func start():
 	enemy.velocity = Vector2.ZERO
 	enemy.collision_layer = 0
 	enemy.collision_mask = 0
+	enemy.set_animation("Die")
